a6621bc6d09203c08624ff32a50e7364dd96679d
kirtihirani <kirtihirani89@gmail.com>
2023-02-22 18:21:55 +0530

    jwt-implement

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 const PORT = 8081;
 
 app.use("/", routes)
+app.use("/userlogin",require('./src/routes/authRoute'))
 
 app.listen(PORT, ()=>{
     console.log("localhost:8081");
